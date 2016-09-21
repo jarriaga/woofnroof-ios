83688980475138e9931d9e2dbf060b503e9a51b4
Manish Anand <manish.anand@erginus.com>
2016-09-21 13:41:04 +0530

    Initial Commit

---
+++ woofnroof-ios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  woofnroof-ios
+//
+//  Created by Manish Anand on 21/09/16.
+//  Copyright © 2016 Prabh Kiran Kaur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ woofnroof-ios/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  woofnroof-ios
+//
+//  Created by Manish Anand on 21/09/16.
+//  Copyright © 2016 Prabh Kiran Kaur. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
